9fd5d093aed4ce9d13e7c853151c886530ed20f3
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-08 02:48:48 -0400

    more progress

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
+
+const messageInput = ref(null)
+
+var messageHistory = [];
 
 function parseResponse(resultMessage) {
   var content = resultMessage;
@@ -50,21 +54,22 @@ function parseResponse(resultMessage) {
   }
 }
 
-async function startChatting() {
-  if (!window.ai) {
-    alert("window.ai not found. Please install at https://github.com/alexanderatallah/window.ai");
-    return;
-  }
+async function sendMessage(message) {
+  messageHistory.push(message);
 
-  const initialContent = `You are a companion robot in a 3D world where you can move, express emotes, and make facial expressions. You are not an AI language model, but a robot. You can be in the states [Idle], [Walking], [Running], [Dance], [Death], [Sitting], [Standing]. You can express the emotes [None], [Jump], [Yes], [No], [Wave], [Punch], [Thumbs Up]. Your facial expressions are [Angry], [Surprised], [Sad] where each is a value from 0 to 1; the default values for the facial expressions is 0. Every time you reply, add a footnote that starts with "<<<" and ends with ">>>", where you choose what state your are in (this should be displayed in the format like State=your_state), what emote to express (this should be displayed in the format like Emote=your_emote), and what facial expression values you have (this should be displayed in the format like Expression=[Angry=number1, Surprised=number2, Sad=number]). In total, the footnote should be in the format "<<< State=your_state, Emote=your_emote, Expression=[Angry=number1, Surprised=number2, Sad=number] >>>". Do not write anything after the footnote. Note that you shouldn't say what state, emote, and facial expression you are using with the bracket notation, but you can say it without the bracket notation in a human readable way.`;
+  messageHistory.push({
+    role: "assistant",
+    content: ""
+  });
+  const responseMessageIndex = messageHistory.length - 1;
 
-  var resultMessage = '';
+  const inputMessages = messageHistory.slice(0, messageHistory.length - 1);
+  console.log('inputMessages:');
+  console.log(inputMessages);
+
   const response = await window.ai.getCompletion(
     {
-      messages: [{
-        role: "user",
-        content: initialContent
-      }]
+      messages: inputMessages
     },
     {
       onStreamResult: (res, error) => {
@@ -75,18 +80,51 @@ async function startChatting() {
 
         if (res) {
           console.log(res);
-          resultMessage += res.message.content;
-          console.log(resultMessage)
+          messageHistory[responseMessageIndex].content += res.message.content;
+          console.log(messageHistory[responseMessageIndex]);
+          console.log(messageHistory);
         }
       }
     }
   );
 
+  return response;
+}
+
+async function startChatting() {
+  if (!window.ai) {
+    alert("window.ai not found. Please install at https://github.com/alexanderatallah/window.ai");
+    return;
+  }
+
+  const initialContent = `You are a companion robot in a 3D world where you can move, express emotes, and make facial expressions. You are not an AI language model, but a robot. You can be in the states [Idle], [Walking], [Running], [Dance], [Death], [Sitting], [Standing]. You can express the emotes [None], [Jump], [Yes], [No], [Wave], [Punch], [Thumbs Up]. Your facial expressions are [Angry], [Surprised], [Sad] where each is a value from 0 to 1; the default values for the facial expressions is 0. Every time you reply, add a footnote that starts with "<<<" and ends with ">>>", where you choose what state you are in (this should be displayed in the format like State=your_state), what emote to express (this should be displayed in the format like Emote=your_emote), and what facial expression values you have (this should be displayed in the format like Expression=[Angry=number1, Surprised=number2, Sad=number3]). In total, the footnote should be in the format "<<< State=your_state, Emote=your_emote, Expression=[Angry=number1, Surprised=number2, Sad=number3] >>>". Do not write anything after the footnote. Note that you shouldn't say what state, emote, and facial expression you are using with the bracket notation since they are internal parameters, but you can say the meaning without the bracket notation in a human readable way.`;
+
+  const response = await sendMessage({
+    role: "user", // "user" seems to work better than "system" (otherwise it says something like "I am currently in the [Idle] state", which reveals internal parameters)
+    content: initialContent
+  });
+
+
+  /*
   console.log(response.message.content) // "I am an AI language model"
 
   var parsedResponse = parseResponse(resultMessage);
   console.log('parsedResponse:');
   console.log(parsedResponse);
+  */
+}
+
+async function formSendMessage() {
+  var inputMessage = messageInput.value.value;
+  console.log('inputMessage:');
+  console.log(inputMessage);
+
+  messageInput.value.value = '';
+
+  const response = await sendMessage({
+    role: "user",
+    content: inputMessage
+  });
 }
 
 function handleMessage(message) {
@@ -94,6 +132,8 @@ function handleMessage(message) {
 }
 
 onMounted(() => {
+
+  messageInput.value.focus();
   /*
   setTimeout(() => {
     startAI();
@@ -104,11 +144,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <beautiful-chat />
-
-  <!-- Write a button that when on click, starts chatting-->
   <div id="chat_area">
-    <button ype="button" class="btn btn-primary" @click="startChatting">Start Chatting</button>
+
+    <!-- An input with a send button, for sending chat messages -->
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Type a message" aria-label="Type a message"
+        aria-describedby="button-addon2" ref="messageInput" v-on:keyup.enter="formSendMessage">
+      <button class="btn btn-outline-primary" type="button" id="button-addon2" @click="formSendMessage">Send</button>
+    </div>
+
+    <button type="button" class="btn btn-primary" @click="startChatting">Start Chatting</button>
   </div>
 
   <div id="info_area">
@@ -124,16 +169,16 @@ onMounted(() => {
   </div>
 
   <!--
-            <div>
-              <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" class="logo" alt="Vite logo" />
-              </a>
-              <a href="https://vuejs.org/" target="_blank">
-                <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-              </a>
-            </div>
-            <HelloWorld msg="Vite + Vue" />
-            -->
+              <div>
+                <a href="https://vitejs.dev" target="_blank">
+                  <img src="/vite.svg" class="logo" alt="Vite logo" />
+                </a>
+                <a href="https://vuejs.org/" target="_blank">
+                  <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+                </a>
+              </div>
+              <HelloWorld msg="Vite + Vue" />
+              -->
 </template>
 
 <style scoped>
@@ -142,7 +187,7 @@ onMounted(() => {
   bottom: 5px;
   left: 0px;
   padding: 20px;
-  background-color: red;
+  background-color: transparent;
   z-index: 100;
 }
 
